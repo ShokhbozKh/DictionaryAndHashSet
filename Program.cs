bf30c54f2eeb3ad3efe13a9f54497f90679e8d63
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-28 06:21:09 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();//key qiymatlari takror yozilsa olinmaydi
+
+            //
+            keyValuePairs.Add(1, "shohboz");
+            keyValuePairs.Add(2,"shoxrux");
+            keyValuePairs.Add(3, "jaga");
+            keyValuePairs.Add(3, "jaga");// kompilyatsiyada xato beradi
+
+
+            foreach(KeyValuePair<int,string> i in keyValuePairs)
+            {
+                Console.WriteLine(i.Key +" " + i.Value);
+            }
+
+            HashSet<string> keys = new HashSet<string>();//Faqat bir marta takrorlanganlarni oladi qayta yozilganlar olinmaydi
+            keys.Add("1");
+            keys.Add("2");
+            keys.Add("2");
+            keys.Add("3");
+            keys.Add("4");
+            foreach(string key in keys)
+            {
+                Console.WriteLine(key);
+            }
+
+
+
+
+        }
+       
+    }    
+}
